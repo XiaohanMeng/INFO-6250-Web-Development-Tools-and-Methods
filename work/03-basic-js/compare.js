8acc88b1468ef71cec8bf80c1c485ff471100093
Xiaohan Meng <meng.xiaoh@northeastern.edu>
2023-10-02 14:52:18 -0700

    Xiaohan Adds homework3-js (already get 2-day extension from Brett)

--- work/03-basic-js/compare.js
+++ work/03-basic-js/compare.js
@@ -6,5 +6,25 @@ function compare( word, guess ) {  // DO NOT MODIFY
 
 /* YOU MAY MODIFY THE LINES BELOW */
 
-  return 0; // this line is wrong
+  // convert to lower case for comparation
+  word = word.toLowerCase();
+  guess = guess.toLowerCase();
+
+  // get all letters in common
+  let commonLetters = 0;
+
+  // get guess letter first
+  for (let i = 0; i < guess.length; i++) {
+    const char = guess.charAt(i);
+
+    for(let j = 0; j < word.length; j++) {
+      const char2 = word.charAt(j);
+      if(char2 === char){
+        commonLetters++;
+        break;
+      }
+    }
+  }
+
+  return commonLetters;
 }
